56322ffb287a39ae6582755f9183ec211862be0b
AdrianScharff <adrian18juarez@gmail.com>
2023-06-19 10:33:06 -0600

    Set sizes and colors for the css menu elements

--- menu.js
+++ menu.js
@@ -1,5 +1,38 @@
-const hamburger = document.querySelector('.hamburger');
+let hamburger = document.querySelector(".hamburger");
+let fullMenu = document.querySelector(".fullMenu");
 
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('close');
-});
+function burgerToClose () {
+  hamburger.classList.toggle("active");
+  fullMenu.classList.toggle("active");
+}
+
+hamburger.addEventListener("click", burgerToClose);
+
+let fullMenuLink = document.querySelectorAll(".fullMenuLink");
+
+function linkCloseTheBlue() {
+  hamburger.classList.remove("active");
+  fullMenu.classList.remove("active");
+}
+
+fullMenuLink.forEach(n => n.addEventListener("click", linkCloseTheBlue));
+
+let contactSection = document.querySelector("#contact");
+let hamburgerBars = document.querySelectorAll(".bar")
+
+function chameleon () {
+    let scrollPosition = window.scrollY || window.pageYOffset;
+    let contactSectionPosition = contactSection.offsetTop;
+
+    if (scrollPosition >= contactSectionPosition) {
+        hamburgerBars.forEach(function(bar) {
+        bar.style.backgroundColor = "white"
+        });
+    } else {
+        hamburgerBars.forEach(function(bar) {
+          bar.style.removeProperty('background-color');
+        })
+    }
+}
+
+window.addEventListener("scroll", chameleon);
