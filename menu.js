9db7042f2b85b2199fcd59fc998c4f0dfb4801a9
AdrianScharff <adrian18juarez@gmail.com>
2023-06-26 09:49:21 -0600

    Add  menu with html, css and JavaScript

--- menu.js
+++ menu.js
@@ -1,4 +1,39 @@
+// This is for the menu
 
+const hamburger = document.querySelector('.hamburger');
+const fullMenu = document.querySelector('.fullMenu');
+const fullMenuLink = document.querySelectorAll('.fullMenuLink');
+const contactSection = document.querySelector('#contact');
+const hamburgerBars = document.querySelectorAll('.bar');
+
+function burgerToClose() {
+  hamburger.classList.toggle('active');
+  fullMenu.classList.toggle('active');
+}
+
+hamburger.addEventListener('click', burgerToClose);
+
+fullMenuLink.forEach((n) => n.addEventListener('click', burgerToClose));
+
+// menu button change color when scrolling over contact section
+function chameleon() {
+  const scrollPosition = window.scrollY || window.pageYOffset;
+  const contactSectionPosition = contactSection.offsetTop;
+
+  // Uncomment the 9 lines below and delete this line
+  
+  // if (scrollPosition >= contactSectionPosition) {
+  //   hamburgerBars.forEach((bar) => {
+  //     bar.style.backgroundColor = 'black';
+  //   });
+  // } else {
+  //   hamburgerBars.forEach((bar) => {
+  //     bar.style.removeProperty('background-color');
+  //   });
+  // }
+}
+
+window.addEventListener('scroll', chameleon);
 
 // This is for the Pop Up Windows
 
